a3c377074b29c186f8e94a8c20e0879dd433f5cb
Joe Hayden <eljoeyy@gmail.com>
2019-10-14 13:43:05 -0500

    finish basic node site

---
+++ index.js
@@ -0,0 +1,29 @@
+let http = require('http')
+let url = require('url')
+let fs = require('fs')
+
+http
+  .createServer((req, res) => {
+    let path = url.parse(req.url, true)
+    let filename = ''
+
+    if (path.pathname === '/') {
+      filename = './index.html'
+    } else if (path.pathname === '/about' || path.pathname === '/contact-me') {
+      filename = '.' + path.pathname + '.html'
+    } else {
+      filename = './404.html'
+    }
+
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        return res.end('404 Not Found')
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.write(data)
+      return res.end()
+    })
+  })
+  .listen(8080)
